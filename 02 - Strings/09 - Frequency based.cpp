e329d2fd44b6fd79c7b0f69bdc2668705765e5a2
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-12-10 14:29:50 +0530

    Update 09 - Frequency based.cpp

--- 02 - Strings/09 - Frequency based.cpp
+++ 02 - Strings/09 - Frequency based.cpp
@@ -65,3 +65,48 @@ int main()
         cout << count;
     }
 }
+
+// Remove whitespaces 
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "My name is raj";
+    string ans;
+    
+    for( int i=0 ; i<str.length() ; i++)
+    {
+        if( str[i] != ' ')
+        {
+            ans += str[i];
+        }
+    }
+    
+    cout << ans;
+}
+
+// Count digit & aplhabets
+
+void countDigitsAndAlphabets(string s) {
+    int alphabets = 0, digits = 0;
+
+    for (char ch : s) {
+        if (isalpha(ch)) alphabets++; // Check if character is an alphabet
+        else if (isdigit(ch)) digits++; // Check if character is a digit
+    }
+
+    cout << "Alphabets: " << alphabets << ", Digits: " << digits << endl;
+}
+
+// Remove special characters 
+
+string removeSpecialCharacters(string s) {
+    string result = "";
+
+    for (char ch : s) {
+        if (isalnum(ch)) result += ch; // Keep only alphabets and digits
+    }
+
+    return result;
+}
