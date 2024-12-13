db6c804a93d2325109027e378cfac27d9118c393
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-12-13 15:47:15 +0530

    Update 01 - Grid unique path.cpp

--- 03 - DP/07 - DP on grid/01 - Grid unique path.cpp
+++ 03 - DP/07 - DP on grid/01 - Grid unique path.cpp
@@ -8,38 +8,38 @@
 // Memoization : Going from bottom to up ( m-1 & n-1 to 0 , 0)
 // We can go in two ways up or left from bottom
 
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
 
-int countWaysUtil(int i, int j, vector<vector<int>>& dp) {
-    if (i == 0 && j == 0)
+    int solve(int i , int j , vector<vector<int>>& dp)
+    {
+         if (i == 0 && j == 0)
         return 1;
 
-    if (i < 0 || j < 0)
-        return 0;
+        if (i < 0 || j < 0)
+            return 0;
 
-    if (dp[i][j] != -1)
-        return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-    int up = countWaysUtil(i - 1, j, dp);
-    int left = countWaysUtil(i, j - 1, dp);
+        int up = solve(i - 1, j, dp);
+        int left = solve(i, j - 1, dp);
 
-    return dp[i][j] = up + left;
-}
+        return dp[i][j] = up + left;
 
-int main() {
-    int m = 3, n = 2;
+    }
 
-    // Create dp table in main function
-    vector<vector<int>> dp(m, vector<int>(n, -1));
+    int uniquePaths(int m, int n) {
 
-    // Calculate the number of ways using dp
-    int result = countWaysUtil(m - 1, n - 1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-    cout << "Number of ways to reach (" << m - 1 << ", " << n - 1 << "): " << result << endl;
-    return 0;
-}
+        int ans = solve(m-1,n-1,dp);
 
+        return ans;
+
+
+    }
+};
 
 /* 
   Steps for Tabulaion 
