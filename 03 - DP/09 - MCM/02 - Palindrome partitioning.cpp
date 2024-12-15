78f2aa6a9bb0917a2fdec974634836a697c90a73
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2024-12-15 13:20:12 +0530

    Create 02 - Palindrome partitioning.cpp

---
+++ 03 - DP/09 - MCM/02 - Palindrome partitioning.cpp
@@ -0,0 +1,96 @@
+/*
+  - Ex : nitin
+  - We want to divide string into min no of partition so that every partition must be palindrome
+  - output : 2 
+  - n | iti | n
+  - Here we had added two partition in between
+
+  - How to identify its MCM
+  - we fix start & end
+  - then we are partitioning in all in between position like k in MCM
+
+  - Steps : 
+  - Find i,j
+  - base case
+  - k loop
+  - apply function on temp ans , to find actual ans
+
+  - Logic : 
+  - we take begin from i=0 to j=s.length() - 1
+  - IF empty string or string length is one then we dont need to do any kind of partition , so return 0
+  - is entire string is palindrome then also return 0
+*/
+
+// Recursive : 
+
+class Solution {
+  public:
+    bool isPalindrome(string &s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int solve(string &s, int i, int j) {
+        if (i >= j || isPalindrome(s, i, j))
+            return 0;
+
+        int mini = INT_MAX;
+
+        for (int k = i; k < j; k++) {
+            int temp = solve(s, i, k) + solve(s, k + 1, j) + 1;
+
+            mini = min(mini, temp);
+        }
+
+        return mini;
+    }
+
+    int palindromicPartition(string &s) {
+        return solve(s, 0, s.length() - 1);
+    }
+};
+
+
+// Memo : 
+
+class Solution {
+  public:
+    bool isPalindrome(string &s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int solve(string &s, int i, int j, vector<vector<int>> &dp) {
+        if (i >= j || isPalindrome(s, i, j))
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int mini = INT_MAX;
+
+        for (int k = i; k < j; k++) {
+            int temp = solve(s, i, k, dp) + solve(s, k + 1, j, dp) + 1;
+
+            mini = min(mini, temp);
+        }
+
+        return dp[i][j] = mini;
+    }
+
+    int palindromicPartition(string &s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1)); 
+        return solve(s, 0, n - 1, dp);
+    }
+};
